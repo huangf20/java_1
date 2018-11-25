e980f5f252482c10a833cac80e12083b318e2b70
huangf20 <38744634+huangf20@users.noreply.github.com>
2018-11-25 21:05:29 +0800

    Create 7.java

---
+++ 7.java
@@ -0,0 +1,12 @@
+public class _7 {
+	public  static void main(String args[]) {
+		char a=(char)176,b=(char)219;
+		System.out.println(b+""+a+""+a+""+a+""+b);
+		System.out.println(a+""+b+""+a+""+b+""+a);
+		System.out.println(a+""+a+""+b+""+a+""+a);
+		System.out.println(a+""+b+""+a+""+b+""+a);
+		System.out.println(b+""+a+""+a+""+a+""+b);
+	
+	}
+
+}
