da995d73a804ca99477a98234c838125ae790aaf
huangf20 <38744634+huangf20@users.noreply.github.com>
2018-11-25 21:03:41 +0800

    Create 6.java

---
+++ 6.java
@@ -0,0 +1,11 @@
+public class _6 {
+	public static void main(String args[]) {
+
+		System.out.println(" ****");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println(" ****");
+		
+	}
+
+}
